0367a8a4e239488c92128ea44a574590951b03d4
Anwesha Das <anwesha068@gmail.com>
2024-08-17 01:23:32 +0530

    Updated

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -5,10 +5,10 @@ import { doc, getDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { toast } from 'sonner';
-import InfoSection from './Components/infoSection';
 import Hotels from './Components/Hotels';
 import PlacesToVisit from './Components/PlacesToVisit';
 import Footer from '@/components/ui/custom/Footer';
+import InfoSection from './Components/InfoSection';
 
 
 function Viewtrip() {
