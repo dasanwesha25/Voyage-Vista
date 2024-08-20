91a936d80e97a0abd4d06b8bab49f7e482a7d11c
Anwesha Das <anwesha068@gmail.com>
2024-08-20 23:20:32 +0530

    Updated

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 import Footer from './Footer'
 function Hero() {
   return (
-    <div className='flex flex-col items-center mt-24 md:pt-32 xl:mt-24 md:mx-56 gap-9'>
-      <h1 className='font-extrabold text-2xl md:text-4xl xl:text-[50px] text-center '>
+    <div className='flex flex-col translate-y-20 xl:-translate-y-16 lg:-translate-y-16 items-center sm:mt-24 md:pt-36 xl:mt-24 md:mx-56 gap-9'>
+      <h1 className='font-extrabold text-xl md:text-4xl xl:text-[50px] text-center lg:pb-4'>
         Unlock New Horizons with <span className='text-[#f56551] font-serif'>VoyageVista</span>
-        <p className=' text-xs sm:text-sm md:text-xl font-medium text-gray-300 text-center md:py-5 sm:py-16 py-24'>
+        <p className=' text-xs sm:text-sm md:text-xl font-medium text-gray-300 text-center md:py-8 sm:py-16 py-11 xl:py-20 lg:pt-12'>
           Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
         </p>
       </h1>
